13931f34ffea10c75a4643447632c7c8acc5cc47
acye <edwinalejandro3016@gmail.com>
2021-11-17 12:53:46 -0400

    trivia 2.0

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 1000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/public/trivia.html');
+})
+
+app.listen(port,(req,res)=>{
+    console.log(port);
+})
+
+
+
+
